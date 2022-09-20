c62457c802a788d99913a4d6f6ba2ed9ff36b546
VInicius Ferreira <contato.vinicius-af@outlook.com>
2022-09-20 19:59:50 -0300

    removendo campos antigos

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,9 +5,6 @@
             <div id="logo"><img  src="../assets/imgs/logo-black.png" alt="50px"></div>
             <h2 class="mb-4">Login como gerenciador</h2>
             <div class="ipt-sub-area mb-4">
-                <!-- <div class="title-ipt">E-mail</div>
-                <input placeholder="E-mail" v-bind:class="{'is-invalid': error.fun_email}" class="ipt form-control form-control-lg" type="email" name="fun_email" v-model="user.fun_email" >
-                <div class="invalid-feedback color-danger" id="fun_email" >{{error.fun_email}}</div> -->
                 <v-text-field
                     v-model="user.fun_email"
                     :type="'email'"
@@ -17,9 +14,6 @@
                 ></v-text-field>
             </div>
             <div class="ipt-sub-area mb-4">
-                <!-- <div class="title-ipt">Senha</div> -->
-                <!-- <input placeholder="Senha" v-bind:class="{'is-invalid': error.fun_senha}"  class="ipt form-control form-control-lg" type="password" name="fun_senha" v-model="user.fun_senha" >
-                <div class="invalid-feedback color-danger" id="fun_senha" >{{error.fun_senha}}</div> -->
                 <v-text-field
                     v-model="user.fun_senha"
                     :type="'password'"
