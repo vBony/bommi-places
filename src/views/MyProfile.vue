58d3606126859806910cdf6c906a36ab1f6fb557
Vinicius de Assis Ferreira <vini_07-ferreira@hotmail.com>
2025-03-03 20:34:19 -0300

    ref #15(meu perfil) - criando aba de dados pessoais

--- src/views/MyProfile.vue
+++ src/views/MyProfile.vue
@@ -1,13 +1,13 @@
 <template>        
-    <v-main class="d-flex mx-lg-4 my-lg-4" style="min-height: 300px;">
+    <v-main class="d-flex" style="min-height: 300px;">
         <v-container fluid>
             <v-row justify="center">
-                <v-col cols="12" md="8" sm="12">
+                <v-col cols="12" md="10" sm="12">
                     <h2>Meu Perfil</h2>
                 </v-col>
 
-                <v-col cols="12" md="8" sm="12">
-                    <v-card class="elevation-4 pa-10">
+                <v-col cols="12" md="10" sm="12">
+                    <v-card class="elevation-5 pa-10">
                         <v-row>
                             <v-tabs v-model="tab">
                                 <v-tab 
@@ -30,66 +30,105 @@
                                 <v-window v-model="tab">
                                     <v-window-item value="1">
                                         <v-row class="px-0 py-0 mt-4">
+                                            <v-col cols="12">
+                                                <v-card class="pa-5" color="surface-variant" variant="tonal">
+                                                    <div class="d-flex justify-space-between align-center">
+                                                        <div class="d-flex align-center">
+                                                            <input 
+                                                                type="file" 
+                                                                style="display: none" 
+                                                                id="avatar"
+                                                                accept="image/jpeg, image/jpg, image/png"
+                                                            >
+                                                            
+                                                            <v-img
+                                                                class="rounded-circle me-5"
+                                                                width="90"
+                                                                height="90"
+                                                                aspect-ratio="1/1"
+                                                                cover
+                                                                src="https://randomuser.me/api/portraits/men/85.jpg"
+                                                            ></v-img>
+                                                            <div cols="12">
+                                                                <div class="font-weight-bold text-truncate"> {{ user.emp_first_name }} </div>
+                                                                <div class="text-disabled text-subtitle-2"> {{ user.emp_type_name }} </div>
+                                                            </div>
+                                                        </div>
+    
+                                                        <div>
+                                                            <v-btn
+                                                                prepend-icon="mdi-camera-plus-outline"
+                                                                color="black"
+                                                                @click="triggerUpload()"
+                                                            >
+                                                                Alterar foto
+                                                            </v-btn>
+                                                        </div>
+                                                    </div>
+                                                </v-card>
+
+                                            </v-col>
+                                        </v-row>
+
+                                        <v-row class="px-0 py-0 mt-4">
                                             <v-col cols="12" lg="6" md="6">
-                                                <v-text-field 
+                                                <app-label text="Nome" />
+                                                <text-field
                                                     v-model="user.emp_first_name"
-                                                    label="Nome" 
-                                                    variant="outlined" 
-                                                    type="text" 
-                                                    hide-details="auto" 
                                                     :error-messages="messages.emp_first_name"
-                                                ></v-text-field>
+                                                />
                                             </v-col>
                 
                                             <v-col cols="12" lg="6" md="6" sm="12">
-                                                <v-text-field 
+                                                <app-label text="Sobrenome" />
+                                                <text-field
                                                     v-model="user.emp_last_name"
-                                                    label="Sobrenome" 
-                                                    variant="outlined" 
-                                                    type="text" 
-                                                    hide-details="auto" 
                                                     :error-messages="messages.emp_last_name"
-                                                ></v-text-field>
+                                                />
                                             </v-col>
                                         </v-row>
 
                                         <v-row class="px-0 py-0 mt-4">
                                             <v-col cols="12">
-                                                <v-text-field 
+                                                <app-label text="CPF" />
+                                                <text-field
                                                     v-model="user.emp_cpf"
-                                                    label="CPF do responsável legal" 
-                                                    variant="outlined"
-                                                    hide-details="auto"
-                                                    type="text"
                                                     :error-messages="messages.emp_cpf"
                                                     v-maska:[mt.cpf]
-                                                ></v-text-field>
+                                                />                                                
                                             </v-col>
                                         </v-row>
 
                                         <v-row class="px-0 py-0 mt-4">
                                             <v-col cols="12">
-                                                <v-text-field 
+                                                <app-label text="Data de nascimento" />
+                                                <text-field
                                                     v-model="user.emp_birthdate"
-                                                    label="Data de nascimento" 
-                                                    variant="outlined"
-                                                    hide-details="auto"
-                                                    type="date"
                                                     :error-messages="messages.emp_birthdate"
-                                                ></v-text-field>
+                                                    type="date"
+                                                />
                                             </v-col>
                                         </v-row>
 
-                                        <v-row class="px-0 py-0 mt-4">
+                                        <v-row class="px-0 py-0 mt-4 mb-4">
                                             <v-col cols="12">
-                                                <v-text-field 
+                                                <app-label text="E-mail" />
+                                                <text-field
                                                     v-model="user.emp_email"
-                                                    label="E-mail" 
-                                                    type="email" 
-                                                    variant="outlined"
-                                                    hide-details="auto" 
                                                     :error-messages="messages.emp_email"
-                                                ></v-text-field>
+                                                    type="email"
+                                                />
+                                            </v-col>
+                                        </v-row>
+
+                                        <v-row>
+                                            <v-col cols="12">
+                                                <v-btn
+                                                    prepend-icon="mdi-pencil-outline"
+                                                    color="black"
+                                                >
+                                                    Salvar Alterações
+                                                </v-btn>
                                             </v-col>
                                         </v-row>
                                     </v-window-item>
@@ -110,19 +149,19 @@
 <script lang='ts'>
 // @ts-nocheck
 import { defineComponent } from 'vue';
-import VerticalMenu from '@/components/VerticalMenu.vue'
-import AppBar from '@/components/AppBar.vue'
 import req from '../helpers/http'
 import { useUserStore } from '../store/user'
 import UserModel from '../entities/User'
 import { useDisplay } from 'vuetify'
 import { vMaska, Mask } from "maska"
 import MaskTokens from '../entities/Masks'
+import TextField from '@/components/fields/TextField.vue';
+import AppLabel from "@/components/fields/LabelField.vue";
         
 const App = defineComponent({
 components: {
-    VerticalMenu,
-    AppBar
+    TextField,
+    AppLabel
 },
 
 directives: { maska: vMaska },
@@ -150,7 +189,10 @@ created(){
 },
 
 methods: {
-    
+    triggerUpload(){
+        let inputFile = document.getElementById("avatar")
+        inputFile.click()
+    }
 },
 
 mounted(){
