ad84b78a5be90515efaefd2390171511cbce71c5
Vinicius de Assis Ferreira <vini_07-ferreira@hotmail.com>
2025-03-04 21:46:14 -0300

    ref #15 (meu perfil) - finalizando aba serviços

--- src/views/MyProfile.vue
+++ src/views/MyProfile.vue
@@ -20,6 +20,12 @@
                                     value="2"
                                     prepend-icon="mdi-store-cog"
                                 >
+                                    Serviços
+                                </v-tab>
+                                <v-tab 
+                                    value="3"
+                                    prepend-icon="mdi-toolbox-outline"
+                                >
                                     Atendimento
                                 </v-tab>
                             </v-tabs>
@@ -29,6 +35,18 @@
                             <v-col cols="12" class="pa-0">
                                 <v-window v-model="tab">
                                     <v-window-item value="1">
+                                        <v-row class="px-0 py-0 mt-4">
+                                            <v-alert
+                                                class="px-10"
+                                                closable
+                                                icon="mdi-information"
+                                                title="Atenção!"
+                                                text="Os dados alterados nesta aba serão aplicados a todos os estabelecimentos nos quais você trabalha"
+                                                type="info"
+                                                variant="tonal"
+                                            ></v-alert>
+                                        </v-row>
+
                                         <v-row class="px-0 py-0 mt-4">
                                             <v-col cols="12">
                                                 <v-card class="pa-5" color="surface-variant" variant="tonal">
@@ -134,7 +152,112 @@
                                     </v-window-item>
 
                                     <v-window-item value="2">
-                                        Two
+                                        <v-row class="px-0 py-0 mt-4">
+                                            <v-col cols="12">
+                                                <h3>Serviços</h3>
+                                                <p class="text-disabled">Defina os serviços que você realiza atendimento</p>
+                                            </v-col>
+                                        </v-row>
+
+                                        <v-row class="px-0 py-0 mt-4">
+                                            <v-col cols="12" lg="4" md="4" sm="12">
+                                                <app-label text="Categoria" />
+                                                <v-select
+                                                    v-model="employeeService.idCategory"
+                                                    label="Selecione uma categoria"
+                                                    no-data-text="Nenhuma categoria encontrada."
+                                                    variant="outlined"
+                                                    density="compact"
+                                                    :items="categories"
+                                                    item-title="case_name" 
+                                                    item-value="case_id"
+                                                    single-line
+                                                    @update:modelValue="getServices()"
+                                                ></v-select>
+                                            </v-col>
+                                            
+                                            <v-col cols="12" lg="4" md="4" sm="12">
+                                                <app-label text="Serviço" />
+                                                <v-select
+                                                    v-model="employeeService.emse_sepl_id"
+                                                    :error-messages="messages.emse_sepl_id"
+                                                    label="Selecione um serviço"
+                                                    no-data-text="Nenhum serviço encontrado."
+                                                    variant="outlined"
+                                                    density="compact"
+                                                    :items="services"
+                                                    item-title="sepl_name" 
+                                                    item-value="sepl_id"
+                                                    single-line
+                                                ></v-select>
+                                            </v-col>
+
+                                            <v-col cols="12" lg="4" md="4" sm="12">
+                                                <app-label text="" /><br>
+                                                <v-btn
+                                                    prepend-icon="mdi-plus"
+                                                    color="black"
+                                                    block
+                                                    :disabled="(employeeService.idCategory == null || employeeService.emse_sepl_id == null)"
+                                                    @click="createEmployeeService()"
+                                                >
+                                                    Adicionar serviço
+                                                </v-btn>
+                                            </v-col>
+
+                                            <v-col cols="12" class="pa-0">
+                                                <v-table
+                                                    height="300px"
+                                                    fixed-header
+                                                    hover
+                                                >
+                                                    <thead>
+                                                        <tr>
+                                                            <th class="text-left" width="280">
+                                                                Nome
+                                                            </th>
+                                                            <th class="text-left" width="280">
+                                                                Categoria
+                                                            </th>
+                                                            <th class="text-left" width="20">
+                                                                
+                                                            </th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr v-if="employeeServices.length === 0">
+                                                            <td colspan="2" class="text-center">Nenhum serviço adicionado</td>
+                                                        </tr>
+                                                        <tr
+                                                            v-else
+                                                            v-for="item in employeeServices"
+                                                            :key="item.usser_id"
+                                                        >
+                                                            <td width="280">{{ item.service_place.sepl_name }}</td>
+                                                            <td width="280">{{ item.service_place.category_service.case_name }}</td>
+                                                            <td width="20">
+                                                                <v-btn 
+                                                                    elevation="0" 
+                                                                    icon="mdi-delete-outline" 
+                                                                    size="small"
+                                                                    @click="openDialogDeleteService(item.emse_id)"
+                                                                ></v-btn>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+
+                                                </v-table>
+                                            </v-col>
+                                        </v-row>
+                                    </v-window-item>
+
+                                    <v-window-item value="3">
+                                        <v-row class="px-0 py-0 mt-4">
+                                            <v-col cols="12">
+                                                <h3>Atendimento</h3>
+                                                <p class="text-disabled">Dados sobre atendimento</p>
+                                            </v-col>
+                                        </v-row>
                                     </v-window-item>
                                 </v-window>
                             </v-col>
@@ -144,6 +267,56 @@
             </v-row>
         </v-container>
     </v-main>
+
+    <SnackBar
+        v-model="snackbar.show"
+        :text="snackbar.text"
+    />
+
+    <v-dialog
+        v-model="deleteServiceDialog"
+        width="auto"
+    >
+        <v-card
+            max-width="600"
+        >
+            <v-card-title class="d-flex justify-space-between align-center">
+                <div class="text-h6 font-weight-black text-medium-emphasis ps-2">
+                    <v-icon class="me-2"> mdi-shape </v-icon>
+                    Desvincular serviço
+                </div>
+
+                <v-btn
+                    icon="mdi-close"
+                    variant="text"
+                    @click="deleteServiceDialog = false"
+                ></v-btn>
+            </v-card-title>
+            <v-card-text>
+                <p>Tem certeza de que deseja desvincular o serviço <b class="text-decoration-underline">{{ employeeServiceDelete.service_place.sepl_name }}</b>?</p>
+                <!-- <p class="text-disabled">
+                    Essa ação é irreversível. <br>
+                </p> -->
+            </v-card-text>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                    text="Cancelar"
+                    variant="tonal"
+                    class="px-4 mr-4"
+                    @click="deleteServiceDialog = false"
+                ></v-btn>
+
+                <v-btn
+                    class="ms-auto"
+                    text="Sim"
+                    color="red"
+                    variant="tonal"
+                    @click="deleteService(employeeServiceDelete.emse_id)"
+                ></v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 </template>
 
 <script lang='ts'>
@@ -157,11 +330,13 @@ import { vMaska, Mask } from "maska"
 import MaskTokens from '../entities/Masks'
 import TextField from '@/components/fields/TextField.vue';
 import AppLabel from "@/components/fields/LabelField.vue";
+import SnackBar from '@/components/SnackBar.vue'
         
 const App = defineComponent({
 components: {
     TextField,
-    AppLabel
+    AppLabel,
+    SnackBar
 },
 
 directives: { maska: vMaska },
@@ -175,7 +350,29 @@ data() {
         
         tab: '1',
         user: new UserModel(),
-        messages: new UserModel()
+
+        categories: [],
+        services: [],
+
+        employeeService: {
+            idCategory: null,
+            emse_sepl_id: null
+        },
+
+        messages: {
+            emse_sepl_id: ""
+        },
+
+        employeeServices: [],
+
+        employeeServiceDelete: {},
+
+        snackbar: {
+            show: false,
+            text: null
+        },
+
+        deleteServiceDialog: false
     };
 },
 
@@ -188,16 +385,87 @@ created(){
     this.user = this.userStore.getUser ?? new UserModel()
 },
 
+mounted(){
+    this.init()
+},
+
 methods: {
     triggerUpload(){
         let inputFile = document.getElementById("avatar")
         inputFile.click()
+    },
+
+    init(){
+        this.getCategories()
+        this.getEmployeeServices()
+    },
+
+    getCategories(){
+        req.get(this.serverUrl+'/api/place/services/categories')
+        .then( (response) => {
+            this.categories = response.data.categories
+        })
+    },
+
+    getServices(){
+        this.employeeService.emse_sepl_id = null
+        this.services = []
+
+        req.get(this.serverUrl+'/api/place/services/'+this.employeeService.idCategory)
+        .then( (response) => {
+            this.services = response.data.services
+        })
+    },
+
+    getEmployeeServices(){
+        req.get(this.serverUrl+'/api/admin/employee/services')
+        .then( (response) => {
+            this.employeeServices = response.data.employeeServices
+        })
+    },
+
+    createEmployeeService(){
+        this.messages = {emse_sepl_id: ""}
+
+        req.post(this.serverUrl+'/api/admin/employee/service/', {employeeService: this.employeeService})
+        .then( (response) => {
+            this.employeeServices.unshift(response.data)
+
+            this.snackBar("Serviço vinculado com sucesso")
+        })
+        .catch( (reason) => {
+            this.messages = reason.response.data.errors
+        })
+    },
+
+    openDialogDeleteService(id){
+        this.deleteServiceDialog = true
+
+        this.employeeServiceDelete = this.employeeServices.find(employeeServices => employeeServices.emse_id === id);
+    },
+
+    snackBar(text){
+        this.snackbar.text = text
+        this.snackbar.show = true
+    },
+
+    deleteService(id){
+        req.delete(this.serverUrl+'/api/admin/employee/service/'+id)
+        .then( () => {
+            const index = this.employeeServices.findIndex(service => service.emse_id === id);
+
+            if (index !== -1) {
+                this.employeeServices.splice(index, 1);
+                
+                this.deleteServiceDialog = false
+                this.snackBar("Serviço desvinculado com sucesso")
+            }
+        })
+        .catch( (reason) => {
+            this.messages = reason.response.data.errors
+        })
     }
 },
-
-mounted(){
-
-}
 });
         
 export default App
